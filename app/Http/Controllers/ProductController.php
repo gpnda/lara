843d116b4994ec8423b78c4241f759f374921fd1
root <root@272793.local>
2020-06-12 16:12:02 +0300

    rasras

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,11 +7,7 @@ use Illuminate\Http\Request;
 class ProductController extends Controller
 {
     public function index(){
-<<<<<<< HEAD
-    	$i = '1234 5 digit';
-=======
-    	$i = 'Hardbass';
->>>>>>> a1735c4e35a43df95b6c13568318f2691e0c1f01
-    	return $i;
+    $i = 'Hardbass';
+    return $i;
     }
 }

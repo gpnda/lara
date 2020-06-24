82a9fa7cb62716c45e15cacbde665b8f10d7957b
MorskoyPes <kaktyc_real_ii@mail.ru>
2020-06-24 15:15:04 +0300

    Our test

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class ProductController extends Controller
 {
     public function index(){
-    	$i = 'Hardbass';
+    	$i = 'Hardbass1111111111111';
     	return $i;
 	}
 }
